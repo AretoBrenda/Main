782dc4afed41097c795d4c32f5021bd14320afbb
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-01 16:31:27 +0300

    Create AddAsManyPrintlns.java

---
+++ AddAsManyPrintlns.java
@@ -0,0 +1,11 @@
+//Adding as many printlns as i wish
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("I am learning Java programming language.");
+    System.out.println("It is awesome!");
+    System.out.println("Yes!");
+    
+  }
+}
