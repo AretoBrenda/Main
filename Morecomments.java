1984832cce9289eb47fdadb87ec65167c57a9833
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-03 17:28:07 +0300

    Create Morecomments.java

---
+++ Morecomments.java
@@ -0,0 +1,5 @@
+/*This is a multiline comment ithis works with longer text
+on different lines*/
+public class Morecommets {
+  public static void main(String[] args) {
+    System.out.println("Java is fun!");
