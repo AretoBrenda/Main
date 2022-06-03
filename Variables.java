948fe68e3a78beec3f9f464500dc010d91db7e9e
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-03 17:35:20 +0300

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,8 @@
+//How to declare a variable
+public Class Variables {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 10;
+    int z = x + y;
+    System.out.println(z);
+      
