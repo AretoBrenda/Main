5161411943f2f9fa037334e9438974449554d812
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-03 18:04:43 +0300

    Create Othertypesdeclared.java

---
+++ Othertypesdeclared.java
@@ -0,0 +1,16 @@
+// if you do not desire your text, int value to be over written use the "final" reserved word
+public class Othertypesdeclared {
+  public static void main(String[] args) {
+    final int = 5;
+    myNum = 20 // this will generate an error response
+      System.out.println(myNum);
+    
+    
+    //A demonstration of how to declare variables of other types
+    myNum = 10;
+    float myFloatNum = 3.88f;
+    char myLetter = 'D';
+    boolean myBool = false;
+    String myText = "Yes";
+    
+    
