78849b7fca6c969e783186715e578bffe26080e7
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-03 17:48:31 +0300

    Create Variablesagain.java

---
+++ Variablesagain.java
@@ -0,0 +1,7 @@
+/*Asigning a new value to an existing variable will over write the existing value
+as seen here*/
+public class Variablesagain {
+  public static void main(String[] args) {
+    int myNum = 10;
+    myNum = 15 // myNum is now 15
+      System.out.println(myNum);
