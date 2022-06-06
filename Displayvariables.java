6563977313682d8b6c416ba52d2a0290d938df18
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-06 11:02:32 +0300

    Create Displayvariables.java

---
+++ Displayvariables.java
@@ -0,0 +1,6 @@
+// println method often used to display variables
+
+public class Displayvariables {
+  public static void main(String[] args) {
+    String name = "Brenda";
+    System.out.println("Hello " + name);
