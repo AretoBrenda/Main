e259c3c68d482c1649e7248d3e05c7ff1b9562ed
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-06 11:27:29 +0300

    Create Declaringvariables.java

---
+++ Declaringvariables.java
@@ -0,0 +1,8 @@
+//Declare a variable you intend to use in your code
+public class Declaringvariables {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 10;
+    int z = 13;
+    System.out.println(x + y + z);
+    // this produces the total value of the numbers stored in the respectuve variables
