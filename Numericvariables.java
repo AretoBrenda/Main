713c6e20901112eae7d5709a680cb0b2ec26d446
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-06 11:19:57 +0300

    Create Numericvariables.java

---
+++ Numericvariables.java
@@ -0,0 +1,6 @@
+// a println*() method printing output for added integer variables
+public class Numericvariables {
+  public static void main(String[] args) {
+    int x = 20;
+    int y = 13;
+    System.out.println(x + y);
