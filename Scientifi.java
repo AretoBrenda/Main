a678bc09765c3628166e35bd2b2d4325f0b1d1a3
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-10 13:44:02 +0300

    Create Scientifi.java

---
+++ Scientifi.java
@@ -0,0 +1,9 @@
+//A floating point number can also be a scientific number with an "e" to indicate the power of 10:
+public class Main {
+  public static void main(String[] args) {
+    float f1 = 35e3f;
+double d1 = 12E4d;
+System.out.println(f1);
+System.out.println(d1);
+  }
+}
