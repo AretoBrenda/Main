7d15b9273006175d917954f6814a449abb746734
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-10 12:15:10 +0300

    Create Variablesasignments.java

---
+++ Variablesasignments.java
@@ -0,0 +1,6 @@
+//You can also assign the same value to multiple variables in one line:
+Public class Main {
+  public static void main(String[] args) {
+    int x, y, z;
+    x = y = z = 50;
+    System.out.println(x + y + z);
