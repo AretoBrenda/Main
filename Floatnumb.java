f18360a591dfdd488996d68c4efad5e14702f8bf
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-10 13:35:45 +0300

    Create Floatnumb.java

---
+++ Floatnumb.java
@@ -0,0 +1,7 @@
+//You should use a floating point type whenever you need a number with a decimal, such as 9.99 or 3.14515.
+public class Main {
+public static void main(String[] args) {
+  float myFloatNum = 5.67f;
+  System.out.println(myFloatNum);
+}
+}
