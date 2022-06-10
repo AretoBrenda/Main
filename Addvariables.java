450761f8923c93f5e98c38a98c1b6c211667df95
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-10 13:10:59 +0300

    Update Addvariables.java

--- Addvariables.java
+++ Addvariables.java
@@ -1,7 +1,9 @@
 // Aprintln() method can also be used while adding a variable to a variable
 public class Addvariable {
-  public static void main(String[] args)
+  public static void main(String[] args) {
   String firstName = "Brenda";
   String lastName = "Areto";
   String fullName = firstName + lastName;
   System.out.println(fullName);
+  }
+}
