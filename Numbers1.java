57a174565241883a862cdce063f6154e7496b6ac
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-10 13:22:54 +0300

    Create Numbers1.java

---
+++ Numbers1.java
@@ -0,0 +1,7 @@
+//The short data type can store whole numbers from -32768 to 32767:
+public class Main {
+  public static void main(String[] args) {
+    short myNum = 1000;
+    System.out.println(myNum);
+  }
+}
