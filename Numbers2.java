bab69362b6f843d02ff8e9a93f87c980b99e047c
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-10 13:27:11 +0300

    Create Numbers2.java

---
+++ Numbers2.java
@@ -0,0 +1,7 @@
+//The int data type can store whole numbers from -2147483648 to 2147483647.
+public class Main {
+  public static void main(String[] args) {
+    int myNum = 200000;
+    System.out.println(myNum);
+  }
+}
