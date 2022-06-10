f731d3b9045c14412d86f88f37ae37b2ec6cf174
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-10 13:40:35 +0300

    Create doublenumb.java

---
+++ doublenumb.java
@@ -0,0 +1,7 @@
+//The float and double data types can store fractional numbers.
+public class Main {
+  public static void main(String[] args) {
+    double myNum = 7.77d;
+    System.out.println(myNum);
+  }
+}
