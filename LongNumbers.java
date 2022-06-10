6b67c2186006018cc0ea443b8a9b207fbbcb9d5d
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-10 13:30:46 +0300

    Create LongNumbers.java

---
+++ LongNumbers.java
@@ -0,0 +1,7 @@
+//The long data type can store whole numbers from -9223372036854775808 to 9223372036854775807
+public class Main {
+  public static void main(String[] arga) {
+    long myNum = 2000000000000L;
+    System.out.println(myNum);
+  }
+}
