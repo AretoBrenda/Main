1035a395b5a4b9049e4ee2089f0e2bf3d98cc226
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-10 13:09:08 +0300

    Create Numbers.java

---
+++ Numbers.java
@@ -0,0 +1,7 @@
+//The byte data type can store whole numbers from -128 to 127.
+public class MyByte {
+  public static void main(String[] args) {
+    byte myNum = 20;
+    System.out.println(myNum);
+  }
+}
