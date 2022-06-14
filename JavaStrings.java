85ed533e6fa7c15dabd1ed600f05670d2952c0e2
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-14 18:30:23 +0300

    Create JavaStrings.java

---
+++ JavaStrings.java
@@ -0,0 +1,10 @@
+//String Length
+public class Main {
+  public static void main(String[] args) {
+    
+String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+System.out.println("The length of the txt string is: " + txt.length());
+  }
+}
+
