f6be6d087151f677f9d21fc844c517acf0998e59
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-14 12:19:38 +0300

    Create Booltype.java

---
+++ Booltype.java
@@ -0,0 +1,9 @@
+// A boolean data type is declared with the boolean keyword and can only take the values true or false:
+public class Main {
+  public static void main(String[] args) {
+    boolean isJavaGreatLanguage = true;
+    boolean isJavaAtree = false;    
+    System.out.println(isJavaFun);
+    System.out.println(isAtree);
+  }
+}
