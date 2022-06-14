692918f992105d49f102cd0799087c63d9332c41
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-14 12:27:26 +0300

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,8 @@
+//The char data type is used to store a single character. 
+public class Character {
+  public static void main{String[] args) {
+    char myClassStream = 'B';
+    System.out.println(myClassStream);
+      }
+         }
+          
