868df36973301a37d57a42e3b32da0530a37c841
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-14 12:33:45 +0300

    Create Characters.java

---
+++ Characters.java
@@ -0,0 +1,10 @@
+//Alternatively, if you are familiar with ASCII values, you can use those to display certain characters:
+public class characters {
+  public static void main(String[] args) {
+    char myVal = 55, myVal1 = 20, myVal2 = 30, myVal3 = 15;
+    System.out.println(myVal);
+     System.out.println(myVal1);
+     System.out.println(myVal2);
+     System.out.println(myVal3);
+  }
+}
