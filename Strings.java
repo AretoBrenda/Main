e6490f3584ef7fda2943c168edc610cfc78a4496
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-14 12:37:38 +0300

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,7 @@
+//he String data type is used to store a sequence of characters (text). String values must be surrounded by double quotes:
+public class Main {
+  public static void main(String[] args) {
+    String myText = "I love life";
+    System.out.println(myText);
+  }
+  }
