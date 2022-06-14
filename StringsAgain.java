13214e5ef47b576dc351bfafc61d230eaf0f7329
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-14 18:39:19 +0300

    Create StringsAgain.java

---
+++ StringsAgain.java
@@ -0,0 +1,8 @@
+// How to change text to UpperCase OR to lowerCase
+public class Main {
+  publuc static void Main(String[] args) {
+    String txt = "I think i love java";
+    System.out.println(txt.toUpperCase());
+    System.out.println(txt.toLowerCase());
+  }
+}
