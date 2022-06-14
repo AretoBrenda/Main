7734cf611763958f8a7685f5e653e195d695a237
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-14 18:53:42 +0300

    Create Try.java

---
+++ Try.java
@@ -0,0 +1,14 @@
+public class Try {
+
+
+	
+			 public static void main(String[] args) {
+				 
+			   System.out.println("\tJ"+ " " + " " + " " + " " + 'a' + " " + " " +'v' + " " + " " + " " + " " +'v' + " " + " " + 'a');
+			 
+			   System.out.println("\tJ"+ " "+ " " + " " +'a' + " " + 'a' + " " + " " + 'v' + " " + " " + " "+ 'v'+ " " + 'a' + " "+ 'a');
+			   
+			   System.out.println( " " + " " + " " + " " + " "+'J'+ " " + " " + 'J' + " " + " " + 'a' + 'a' + 'a' + 'a' + 'a' + " " + " " +   'V' + " " + 'V' + " " + 'a' + 'a' + 'a' + 'a' + 'a');
+			   System.out.println(" " + " " + " " + " " + " " + " " + 'J' + 'J' + " " + " " + 'a' + " "+ " " + " " + " " + " " + 'a' + " " + " " + 'V' + " "  + " " + 'a' + " " + " " + " " + " " + 'a');
+					 }
+			 }
