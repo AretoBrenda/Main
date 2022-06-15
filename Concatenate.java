7fef1a879ae25913e2b7f7a7dbcaa76a5366aff9
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-15 19:42:46 +0300

    Create Concatenate.java

---
+++ Concatenate.java
@@ -0,0 +1,9 @@
+//You can also use the concat() method to concatenate two strings:
+
+public class Main {
+  public static void main(String[] args) {
+    String firstName = "Brenda";
+    String lastName = "Areto";
+    System.out.println(firstName.concat(lastName));
+    }
+     }
