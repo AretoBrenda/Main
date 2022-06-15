5640b8f10f2e74cfd54c058ed5313bcfacb2ad3e
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-15 19:35:49 +0300

    Create Concatenation.java

---
+++ Concatenation.java
@@ -0,0 +1,8 @@
+//The + operator can be used between strings to combine them. This is called concatenation:
+public class Main {
+  public static void main(String[] args) {
+    String firstName = "Brenda";
+    String lastName = "Areto";
+    System.out.println(firstName + " " + lastName);
+  }
+}
