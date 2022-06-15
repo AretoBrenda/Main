6d7901de8a5526db190a90fd2cac2ad2ee78768d
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-15 19:24:16 +0300

    Create FindIndexPosition.java

---
+++ FindIndexPosition.java
@@ -0,0 +1,7 @@
+//The indexOf() method returns the index (the position) of the first occurrence of a specified text in a string (including whitespace):
+public class Main {
+  public static void main(string[] args) {
+    String txt = "I love pork";
+    System.out.println(txt.indexOf("love")); // the output will be 3
+  }
+}
