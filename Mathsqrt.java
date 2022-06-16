965a8ea38be878b0f5dc5d002c282c9928da7b93
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-16 20:09:30 +0300

    Create Mathsqrt.java

---
+++ Mathsqrt.java
@@ -0,0 +1,9 @@
+//The Math.sqrt(x) method returns the square root of x:
+//Example
+
+public class Main {
+  public static void main(String[] args) {
+    int x = 80;
+  System.out.println(Math.sqrt(x));
+  }
+}
