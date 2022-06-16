49b24d71df2ad474368645985ffb27631a9e7abf
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-16 19:56:52 +0300

    Create Mathmax.java

---
+++ Mathmax.java
@@ -0,0 +1,9 @@
+//The Math.max() method in Java is used to find the highest value given numbers.
+//For example
+public class Main {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 5;
+    System.out.println( Math.max(x,y));
+  }
+}
