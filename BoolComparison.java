f19ac15a640dfc71c1f36fc2cee93e5fd297168d
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-16 20:24:17 +0300

    Create BoolComparison.java

---
+++ BoolComparison.java
@@ -0,0 +1,9 @@
+//You can use a comparison operator, such as the greater than (>) operator to find out if an expression (or a variable) is true:
+
+public class Main {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 9;
+    System.out.println(x > y); // returns true, because 10 is higher than 9  
+  }
+}
