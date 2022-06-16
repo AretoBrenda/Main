325c086fe67b2f8c2689fa43d688d13d70ed9167
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-16 20:16:42 +0300

    Create Mathabs.java

---
+++ Mathabs.java
@@ -0,0 +1,8 @@
+//The Math.abs(x) method returns the absolute (positive) value of x:
+//example
+public class Main {
+  public static void main(String[] args) {
+    double y = -6.7;
+    System.out.println(Math.abs(y));
+  }
+}
