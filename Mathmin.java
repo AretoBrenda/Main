b531407b30e8d366a961ae5c1ac3ddb78d2c3b74
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-16 20:02:18 +0300

    Create Mathmin.java

---
+++ Mathmin.java
@@ -0,0 +1,9 @@
+// The Math.min() method is used to fine the minimum value of a given numbers
+//example
+public class main {
+  public startic void main(String[] args) {
+    int x = 10; 
+    int y = 5;
+    System.out.println(Math.min(x,y));
+    }
+      }
