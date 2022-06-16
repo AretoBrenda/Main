f2193feace1e28f94e38009ba696d923f1bf080a
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-16 20:27:03 +0300

    Create BoolEquate.java

---
+++ BoolEquate.java
@@ -0,0 +1,8 @@
+//In the examples below, we use the equal to (==) operator to evaluate an expression:
+
+public class Main {
+  public static void main(String[] args) {
+    int x = 10;
+    System.out.println(x == 10); // returns true, because the value of x is equal to 10
+  }
+}
