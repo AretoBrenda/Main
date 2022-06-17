b110c79bb989a255babfe1800e6d182e1bbd58ce
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-17 15:17:43 +0300

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,30 @@
+//Use the switch statement to select one of many code blocks to be executed.
+
+public class Main {
+  public static void main(String[] args) {
+    int months = 4;
+    switch (months) {
+      case 1:
+        System.out.println("January");
+        break;
+      case 2:
+        System.out.println("February");
+        break;
+      case 3:
+        System.out.println("March");
+        break;
+      case 4:
+        System.out.println("April");
+        break;
+      case 5:
+        System.out.println("May");
+        break;
+      case 6:
+        System.out.println("June");
+        break;
+      case 7:
+        System.out.println("July");
+        break;
+    }
+  }
+}
