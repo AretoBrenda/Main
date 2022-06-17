fd2ba965dc4f96247d88e1a04625c6eafc5217ae
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-17 14:23:44 +0300

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,10 @@
+//Use the if statement to specify a block of Java code to be executed if a condition is true.
+public class Main {
+  public static void main(String[] args) {
+    int x = 20;
+    int y = 18;
+    if (x > y) {
+      System.out.println("x is greater than y");
+    }  
+  }
+}
