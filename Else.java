47da7419eda4b96249abc2f982be002fe2d90336
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-17 14:35:17 +0300

    Create Else.java

---
+++ Else.java
@@ -0,0 +1,11 @@
+//Use the else statement to specify a block of code to be executed if the condition is false.
+public class Main {
+public static void main(String[] args) {
+int y = 20;
+if( y < 18) {
+System.out.println("Good Day");
+} else {
+System.out.println("Good evening");
+}
+ }
+ }
