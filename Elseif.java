f0ae7c9ec6bdcb465ec5b8a38fb7a553be5eca43
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-17 14:52:01 +0300

    Create Elseif.java

---
+++ Elseif.java
@@ -0,0 +1,14 @@
+//Use the else if statement to specify a new condition if the first condition is false.
+
+public class Main {
+public static void main(String[] args) {
+int T = 22;
+if(T < 10) {
+System.out.println("Good morning.");
+} else if (T < 20) {
+  System.out.println("Good day.");
+    }  else {
+      System.out.println("Good evening.");
+    }
+  }
+}
