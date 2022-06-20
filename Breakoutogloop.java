f470415d259a5ac4c2459851106a7a583a65abf8
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-20 19:11:24 +0300

    Create Breakoutogloop.java

---
+++ Breakoutogloop.java
@@ -0,0 +1,13 @@
+//The break statement can also be used to jump out of a loop.
+
+public class Main {
+  public static void main(String[] args) {
+    for(int i = 0; i < 10; i++) {
+      if(i == 4) {
+        break;
+      }
+        System.out.println(i);
+      }
+    }
+  }
+
