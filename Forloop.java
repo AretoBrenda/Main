be5d173ce54230b75dec7ea47ca88c5cc37e3b8a
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-20 18:40:11 +0300

    Create Forloop.java

---
+++ Forloop.java
@@ -0,0 +1,9 @@
+//When you know exactly how many times you want to loop through a block of code, use the for loop instead of a while loop:
+
+public class Main {
+  public static void main(String[] args) {
+    for(int i = 0; i<8; i++) {
+      System.out.println(i);
+      }
+  }
+}
