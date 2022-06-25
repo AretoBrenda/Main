4480ae79a4490df5b09a1c7cc4f1419232d7c57d
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-25 13:50:06 +0300

    Create ForEachloopArray.java

---
+++ ForEachloopArray.java
@@ -0,0 +1,10 @@
+//There is also a "for-each" loop, which is used exclusively to loop through elements in arrays:
+
+public class Main {
+  public static void main(String[] args) {
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    for (String i : cars) {
+      System.out.println(i);
+    }    
+  }
+}
