d3377d08ed6efd9eca85a29cfdf91402eb54a5ae
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-25 13:38:40 +0300

    Create ChangevalueArray.java

---
+++ ChangevalueArray.java
@@ -0,0 +1,11 @@
+//To change the value of a specific element, refer to the index number:
+
+public class Main {
+  public static void main(String[] args) {
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    cars[3] = "Opel";
+    System.out.println(cars[3]);
+  }
+}
+
+// Mazda will be replaced by Opel
