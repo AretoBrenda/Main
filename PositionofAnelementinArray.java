0eca518c33cd16a17d1713fc5232b6dbcb1ab97f
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-25 13:33:34 +0300

    Create PositionofAnelementinArray.java

---
+++ PositionofAnelementinArray.java
@@ -0,0 +1,9 @@
+//This statement accesses the value of the first element in cars:
+
+public class Main {
+  public static void main(String[] args) {
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    System.out.println(cars[1]);
+  }
+}
+// the output will be BMW
