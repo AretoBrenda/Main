2565e8d8b5e2236d6a0b97b48ef30005501b3003
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-27 16:21:44 +0300

    Create CallmethodMultiple.java

---
+++ CallmethodMultiple.java
@@ -0,0 +1,13 @@
+//A method can also be called multiple times:
+public class Main {
+  static void myMethod() {
+    System.out.println("I just got executed!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+    myMethod();
+    myMethod();
+  }
+}
+// it will execute the number of times its been called
