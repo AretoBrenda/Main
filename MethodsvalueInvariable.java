b89a9478f195e3bab4e4795350e6d9cb7e292052
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-27 17:09:57 +0300

    Create MethodsvalueInvariable.java

---
+++ MethodsvalueInvariable.java
@@ -0,0 +1,12 @@
+//You can also store the result in a variable (recommended, as it is easier to read and maintain):
+
+public class Main {
+  static int myMethod(int x, int y) {
+    return x + y;
+  }
+
+  public static void main(String[] args) {
+    int z = myMethod(5, 3);
+    System.out.println(z);
+  }
+}
