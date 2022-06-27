1ad502a4e1cc020c74b5913fc812a0963be530e4
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-27 16:39:18 +0300

    Create Methods&parameters.java

---
+++ Methods&parameters.java
@@ -0,0 +1,18 @@
+//method that takes a String called fname as parameter. When the method is called, 
+//we pass along a first name, which is used inside the method to print the full name
+public class Main {
+  static void myMethod(String fname) {
+    System.out.println(fname + " " + "Okotkotber");
+  }
+    public static void main(String[] args) {
+      myMethod("Mary");
+      myMethod("Danny");
+      myMethod("Grace");
+    }
+  }
+
+//output
+//Mary Okotkotber
+//Danny Okotkotber
+//Grace Okotkotber
+      
