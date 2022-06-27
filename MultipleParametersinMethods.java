1f086c52bc13e08771d44642a411053cafd98d84
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-27 16:49:57 +0300

    Create MultipleParametersinMethods.java

---
+++ MultipleParametersinMethods.java
@@ -0,0 +1,13 @@
+//You can have as many parameters as you like:
+
+public class Main {
+  static void myMethod(String fname, int age) {
+    System.out.println(fname + " is " + age);
+  }
+  public static void main(String[] args) {
+    myMethod("Mary", 10);
+    myMethod("Jonnie", 35);
+    myMethod("Brenda", 39);
+  }
+}
+//prints out Name plus age
