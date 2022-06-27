930b1b643fdb9b43be79747dbbec73efd492db03
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-27 16:17:17 +0300

    Create CallMethod.java

---
+++ CallMethod.java
@@ -0,0 +1,11 @@
+//Inside main, call the myMethod() method:
+
+public class Main {
+  static void myMethod() {
+    System.out.println("Still learning java!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+  }
+}
