f2ba263f72524668ba2962fea804efbb98600a40
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-06-30 15:09:18 +0300

    Create Javarecursion.java

---
+++ Javarecursion.java
@@ -0,0 +1,14 @@
+//Use recursion to add all of the numbers up to 10.public class Main {
+  public static void main(String[] args) {
+    int result = sum(10);
+    System.out.println(result);
+  }
+  public static int sum(int k) {
+    if (k > 0) {
+      return k + sum(k - 1);
+    } else {
+      return 0;
+    }
+  }
+}
+
