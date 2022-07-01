4df6184aaa20c8929b6da5622619f194db976942
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-01 20:01:25 +0300

    Create OveridingexistingValues.java

---
+++ OveridingexistingValues.java
@@ -0,0 +1,11 @@
+//Change the value of x from 10 to 25
+
+public class Main {
+  int x = 10;
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    myObj.x = 25;
+    System.out.println(myObj.x);
+  }
+}
+    
