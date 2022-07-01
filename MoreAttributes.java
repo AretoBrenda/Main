f80464ce882ad7b29295d1209d0d7a26469415ca
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-01 20:24:37 +0300

    Create MoreAttributes.java

---
+++ MoreAttributes.java
@@ -0,0 +1,13 @@
+//You can specify as many attributes as you want:
+
+public class Main {
+  String fname = "Agnes";
+  String lname = "Adyao";
+  int age = 39;
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+    System.out.println("Age: " + myObj.age);
+    
+  }
+}
