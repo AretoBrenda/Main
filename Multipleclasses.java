00de02bfd817193d4037f2b42a3aee31de35094e
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-01 19:42:37 +0300

    Create Multipleclasses.java

---
+++ Multipleclasses.java
@@ -0,0 +1,15 @@
+// Creating a project with multiple classes 
+
+//Main class
+public class Main {
+  int y = 20;
+  
+  
+  //Second class
+  class Second {
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println(myObj.y);
+  }
+  }
+} 
