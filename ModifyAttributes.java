a211ba4d876880a66de436822c98bc662a8ca9fc
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-01 19:56:14 +0300

    Create ModifyAttributes.java

---
+++ ModifyAttributes.java
@@ -0,0 +1,10 @@
+//You can also modify attribute values:
+
+public class Main {
+  int x;
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    myObj.x = 10;
+    System.out.println(myObj.x);
+  }
+}
