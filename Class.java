76fb83bdc39acbe158fe6caa223ca07c79ea4b1f
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-01 18:55:43 +0300

    Create Class.java

---
+++ Class.java
@@ -0,0 +1,10 @@
+//Create an object called "myObj" and print the value of x:
+
+
+public class Main {
+    int x = 7;
+  public static void main(String[] args) {
+  Main myObj = new Main();
+    System.out.println(myObj.x);
+  }
+}
