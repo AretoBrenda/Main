bad7120858eb7885ab7e40fbf2f78fc8dae438ba
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-01 19:48:19 +0300

    Create ClassAttributes.java

---
+++ ClassAttributes.java
@@ -0,0 +1,10 @@
+//The following example will create an object of the Main class, with the name myObj. We use the x attribute on the object to print its value:
+
+public class Main {
+  int p = 5;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println(myObj.p);
+  }
+}
