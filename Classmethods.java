1c0c61d0dc4620874c6f5a6dc3354802054f80d1
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-04 17:16:34 +0300

    Create Classmethods.java

---
+++ Classmethods.java
@@ -0,0 +1,11 @@
+//In the following example, myMethod() is used to print a text (the action), when it is called
+
+
+public class Main {
+  static void myMethod() {
+    System.out.println("I am being executed");
+  }
+    public static void main(String[] args) {
+      myMethod();
+    }
+}
