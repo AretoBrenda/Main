e4a8261da475d7e384795f3bce42901c725223ab
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-04 17:32:22 +0300

    Create Methodparameters.java

---
+++ Methodparameters.java
@@ -0,0 +1,11 @@
+// Parameters are information passed in the methods parenthesis.
+public class Main {
+  static void myMethod(String fname) {
+    System.out.println(fname+ " " + "Okotkotber");
+  }
+  public static void main(String[] args) {
+    myMethod("Agnes");
+    myMethod("Caroline");
+    myMethod("Andrew");
+  }
+}
