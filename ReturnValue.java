56cc2d843dbf6cbf6846152c873f8c55434b7ed4
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-04 17:58:56 +0300

    Create ReturnValue.java

---
+++ ReturnValue.java
@@ -0,0 +1,10 @@
+//This example returns the sum of a method's two parameters:
+
+public class Main {
+  static int myMethod(int x, int y) {
+    return x + y;
+  }
+  public static void main(String[] args) {
+    System.out.println(myMethod(5, 4));
+  }
+}
