0d1fcc4ed737b26914a4c1d3ab81e5cdac6d293e
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-04 18:14:43 +0300

    Create MethodWithifElse.java

---
+++ MethodWithifElse.java
@@ -0,0 +1,14 @@
+//It is common to use if...else statements inside methods:
+public class Main {
+  static void checkAge(int age) {
+    if(age < 18) {
+      System.out.println("Access denied- You are not old enough");
+            }else{
+    System.out.println("Access granted. You are old enough");
+                        
+       }
+          }
+          public static void main(String[] args) {
+            checkAge(20);
+          }
+     }
