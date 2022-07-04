9cd27bafe4e27822e2b6bc7908e9d7dcae20a035
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-04 17:47:01 +0300

    Create MultipleParainMethods.java

---
+++ MultipleParainMethods.java
@@ -0,0 +1,12 @@
+// you can use multiple parameters
+public class Main {
+  static void myMethod(String fname, int age) {
+    
+    System.out.println(fname+ " is" + " " + age);
+  }
+  public static void main(String[] args) {
+    myMethod("Agnes", 25);
+    myMethod("Caroline", 20);
+    myMethod("Andrew", 15);
+  }
+}
