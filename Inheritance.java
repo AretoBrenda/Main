cf8c6305bfe02359a19ef18fce9b5e189184434a
AretoBrenda <104834285+AretoBrenda@users.noreply.github.com>
2022-07-07 16:15:00 +0300

    Create Inheritance.java

---
+++ Inheritance.java
@@ -0,0 +1,23 @@
+//In the example below, the Car class (subclass) inherits the attributes and methods from the Vehicle class (superclass):
+
+class Vehicle {
+  protected String brand = "Ford";
+  
+  public void honk() {
+    System.out.println("Tuut, tuut!");
+  }
+}
+  class Car extends Vehicle {
+    
+  private String modelName = "Mustang"; 
+  
+  public static void main(String[] args) {
+    
+    Car myFastCar = new Car();
+    
+    myFastCar.honk();
+    
+    System.out.println(myFastCar.brand + " " + myFastCar.modelName);
+  }
+  }
+ 
